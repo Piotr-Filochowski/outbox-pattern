916e83859590fb8fdebdececa9893c07322cd324
Piotr Filochowski <piotr.filochowski01@gmail.com>
2024-09-13 20:58:03 +0200

    Init commit

---
+++ src/main/kotlin/com/filochowski/outbox/OutboxApplication.kt
@@ -0,0 +1,11 @@
+package com.filochowski.outbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OutboxApplication
+
+fun main(args: Array<String>) {
+	runApplication<OutboxApplication>(*args)
+}
---
+++ src/test/kotlin/com/filochowski/outbox/TestOutboxApplication.kt
@@ -0,0 +1,9 @@
+package com.filochowski.outbox
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+	fromApplication<OutboxApplication>().with(TestcontainersConfiguration::class).run(*args)
+}
