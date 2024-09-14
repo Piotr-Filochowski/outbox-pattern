b5e193f3be4c083edf70cbf2475cce81d50b3b26
Piotr Filochowski <piotr.filochowski01@gmail.com>
2024-09-14 12:22:43 +0200

    Adding dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-	kotlin("jvm") version "2.0.20"
-	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("plugin.jpa") version "1.9.25"
+	kotlin("jvm") version "2.0.20"
+	kotlin("plugin.spring") version "1.9.25"
+	id("groovy")
+	id("com.gorylenko.gradle-git-properties") version "2.4.2"
+	`java-library`
 }
 
 group = "com.filochowski"
@@ -20,11 +22,30 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// spring
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
+
+	// jackson
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+
+	// database
 	runtimeOnly("org.postgresql:postgresql")
+	implementation("com.zaxxer:HikariCP")
+
+	// flyway
+	implementation("org.flywaydb:flyway-database-postgresql")
+
+	// exposed
+	implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.54.0")
+
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
